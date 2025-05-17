c372e42f582baedf2623f4f4af00e51e722a3944
Pedro Escudero Murcia <f92esmup@Pedros-MacBook-Air.local>
2025-05-17 18:05:07 +0200

    desastre

--- src/agent/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Inicializa el módulo del agente de Reinforcement Learning.
-Expone las clases principales para que sean accesibles desde fuera.
-"""
-
-from src.agent.custom_transformer_extractor import CustomTransformerFeatureExtractor
-from src.agent.rl_agent_manager import RLAgentManager
-
-__all__ = ['CustomTransformerFeatureExtractor', 'RLAgentManager']
